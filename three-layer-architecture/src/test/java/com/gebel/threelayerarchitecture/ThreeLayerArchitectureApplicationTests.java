9ea810fc3abd4efec3941cb05d38ccb35970406b
kevin.gebel <gebel.kevin@gmail.com>
2022-09-04 13:04:45 +0200

    Temp

---
+++ three-layer-architecture/src/test/java/com/gebel/threelayerarchitecture/ThreeLayerArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gebel.threelayerarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeLayerArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
