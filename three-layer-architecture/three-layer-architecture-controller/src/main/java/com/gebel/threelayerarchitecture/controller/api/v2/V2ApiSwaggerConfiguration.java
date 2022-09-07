797df49ad14b671f1fcf30737de31e25eba7257d
kevin.gebel <gebel.kevin@gmail.com>
2022-09-07 19:35:36 +0200

    Fix poms

--- three-layer-architecture/three-layer-architecture-controller/src/main/java/com/gebel/threelayerarchitecture/controller/api/v2/V2ApiSwaggerConfiguration.java
+++ three-layer-architecture/three-layer-architecture-controller/src/main/java/com/gebel/threelayerarchitecture/controller/api/v2/V2ApiSwaggerConfiguration.java
@@ -47,7 +47,7 @@ class V2ApiSwaggerConfiguration {
 	
 	private class ReplaceApiBusinessErrorCodeEnumValuesInApiResponses implements OperationCustomizer {
 		
-		private final String API_BUSINESS_ERROR_CODE_PREFIX = ApiBusinessErrorCodeDto.class.getSimpleName() + ".";
+		private static final String API_BUSINESS_ERROR_CODE_PREFIX = ApiBusinessErrorCodeDto.class.getSimpleName() + ".";
 		
 		@Override
 		public Operation customize(Operation operation, HandlerMethod handlerMethod) {
