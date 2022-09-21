beeb92264bf6122491019b59a25b663dff6265ae
kevin.gebel <gebel.kevin@gmail.com>
2022-09-21 08:44:49 +0200

    Kafka sandbox ok

--- three-layer-architecture-sandbox/src/test/java/test/com/gebel/threelayerarchitecture/sandbox/container/TestContainersZookeeperKafkaIT.java
+++ three-layer-architecture-sandbox/src/test/java/test/com/gebel/threelayerarchitecture/sandbox/container/TestContainersZookeeperKafkaIT.java
@@ -1,11 +1,14 @@
 package test.com.gebel.threelayerarchitecture.sandbox.container;
 
+import static org.awaitility.Awaitility.waitAtMost;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -15,7 +18,6 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -34,64 +36,62 @@ class TestContainersZookeeperKafkaIT extends AbstractIntegrationTest {
 	@Test
 	void givenValidKafkaMessage_whenAddingMessageToKafka_thenMessageRetrieved() {
 		// Given
+		String messageId = "message-id";
 		String message = "This is a test message!";
 		
 		// When
-		addMessageToKafka(message);
+		addMessageToKafka(messageId, message);
 		
 		// Then
-		List<String> messages = getKafkaMessages();
+		Map<String, String> messages = getKafkaMessages();
 		assertEquals(1, messages.size());
-		assertEquals("This is a test message!", messages.get(0));
+		assertEquals("This is a test message!", messages.get("message-id"));
 	}
 	
 	@SneakyThrows
-	private void addMessageToKafka(String message) {
-		String host = getTestContainersManager().getTestContainers().getZookeeperKafkaTestContainers().getHost();
-		int port = getTestContainersManager().getTestContainers().getZookeeperKafkaTestContainers().getPort();
-		String bootstrapServers = host + ":" + port;
+	private void addMessageToKafka(String messageId, String message) {
+		String bootstrapServers = getTestContainersManager().getTestContainers().getZookeeperKafkaTestContainers().getContainer().getBootstrapServers();
 		
 		Properties properties = new Properties();
 		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		properties.put(ProducerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
 		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		
-		ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(TOPIC, message);
+		ProducerRecord<String, String> producerRecord = new ProducerRecord<>(TOPIC, messageId, message);
 		
 		try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties)) {
 			kafkaProducer.send(producerRecord).get();
 		}
 	}
 	
-	private List<String> getKafkaMessages() {
-		String host = getTestContainersManager().getTestContainers().getZookeeperKafkaTestContainers().getHost();
-		int port = getTestContainersManager().getTestContainers().getZookeeperKafkaTestContainers().getPort();
-		String bootstrapServers = host + ":" + port;
-		
+	private Map<String, String> getKafkaMessages() {
+		String bootstrapServers = getTestContainersManager().getTestContainers().getZookeeperKafkaTestContainers().getContainer().getBootstrapServers();
+
 		Properties properties = new Properties();
 		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		properties.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		
 		try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties)) {
 			kafkaConsumer.subscribe(List.of(TOPIC));
-			List<String> messages = new ArrayList<>();
-			Awaitility.waitAtMost(Duration.ofSeconds(2))
+			Map<String, String> messages = new HashMap<>();
+			waitAtMost(Duration.ofSeconds(2))
 				.alias("Find messages in Kafka")
 				.until(() -> {
-					List<String> currentMessages = pollMessages(kafkaConsumer);
-					return !currentMessages.isEmpty();
+					messages.putAll(pollMessages(kafkaConsumer));
+					return !messages.isEmpty();
 				});
 			return messages;
 		}
 	}
 	
-	private List<String> pollMessages(KafkaConsumer<String, String> kafkaConsumer) {
+	private Map<String, String> pollMessages(KafkaConsumer<String, String> kafkaConsumer) {
 		ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
-		List<String> messages = new ArrayList<>();
-		records.forEach(record -> messages.add(record.value()));
+		Map<String, String> messages = new HashMap<>();
+		records.forEach(record -> messages.put(record.key(), record.value()));
 		return messages;
 	}
 	
